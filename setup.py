6e0dc3add08ca2f4313d760e781fadaa7922349b
polyccon <polyccon@gmail.com>
2025-02-04 23:45:15 +0000

    feat: adds setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Python package',
+    version='0.1',
+    description='First version of python package',
+    author='Constantina',
+    author_email='polyccon@gmail.com',
+    packages=find_packages(),
+)
