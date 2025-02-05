a128fc63da7311e6cad4347361127d7d96ddbdaf
polyccon <polyccon@gmail.com>
2025-02-05 00:23:16 +0000

    chore: update setup

--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
 from setuptools import setup, find_packages
 
+
 setup(
-    name='Python package',
-    version='0.1',
-    description='First version of python package',
-    author='Constantina',
-    author_email='polyccon@gmail.com',
+    name="simplemath",
+    version="0.1.0",
     packages=find_packages(),
+    install_requires=[],
+    author="Constantina",
+    author_email="polyccon@gmail.com",
+    description="A simple math operations package",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/polyccon/python-packages",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires=">=3.11",
 )
